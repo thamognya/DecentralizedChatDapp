112ed147cf43ef91253792d3f2b53a4712c84b51
Thamognya Kodi <contact@thamognya.com>
2022-06-23 06:48:31 +0700

    update

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const Gun = require('gun');
 const app = express()
 const port = 3005
-const hostname = '192.168.1.122'
-app.use(Gun.serve);
+const hostname = '0.0.0.0'
 
+app.use(Gun.serve);
 const server = app.listen(port, hostname)
 
 Gun({ web: server })
