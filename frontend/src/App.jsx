a556bdd841c70406355e8d2a4cf28f0ad2d85142
Thamognya Kodi <contact@thamognya.com>
2022-06-22 20:46:11 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,88 +1,93 @@
-import './App.scss'
 import { useEffect, useState, useReducer } from 'react'
 import Gun from 'gun'
 
 // initialize gun locally
 const gun = Gun({
-    peers: ['http://0.0.0.0:3003/gun']
+  peers: [
+    'http://192.168.1.122:3005/gun'
+  ]
 })
 
 // create the initial state to hold the messages
 const initialState = {
-    messages: []
+  messages: []
 }
 
 // Create a reducer that will update the messages array
 function reducer(state, message) {
-    return {
-        messages: [message, ...state.messages]
-    }
+  return {
+    messages: [message, ...state.messages]
+  }
 }
 
 export default function App() {
-    // the form state manages the form input for creating a new message
+  // the form state manages the form input for creating a new message
     const [formState, setForm] = useState({
-        name: '',
-        message: ''
+    name: '', message: ''
+  })
+
+  // initialize the reducer & state for holding the messages array
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // when the app loads, fetch the current messages and load them into the state
+  // this also subscribes to new data as it changes and updates the local state
+  useEffect(() => {
+    const messages = gun.get('messages')
+    messages.map().on(m => {
+      dispatch({
+        name: m.name,
+        message: m.message,
+        createdAt: m.createdAt
+      })
     })
+  }, [])
 
-    // initialize the reducer & state for holding the messages array
-    const [state, dispatch] = useReducer(reducer, initialState)
+  // set a new message in gun, update the local state to reset the form field
+  function saveMessage() {
+    const messages = gun.get('messages')
+    messages.set({
+      name: formState.name,
+      message: formState.message,
+      createdAt: Date.now()
+    })
+    setForm({
+      name: '', message: ''
+    })
+  }
 
-    // when the app loads, fetch the current messages and load them into the state
-    // this also subscribes to new data as it changes and updates the local state
-    useEffect(() => {
-        const messages = gun.get('messages')
-        messages.map().on(m => {
-            dispatch({
-                name: m.name,
-                message: m.message,
-                createdAt: m.createdAt
-            })
-        })
-    }, [])
+  function deleteMessage() {
+  }
 
-    // set a new message in gun, update the local state to reset the form field
-    function saveMessage() {
-        const messages = gun.get('messages')
-        messages.set({
-            name: formState.name,
-            message: formState.message,
-            createdAt: Date.now()
-        })
-        setForm({
-            name: '',
-            message: ''
-        })
-    }
+  // update the form state as the user types
+  function onChange(e) {
+    setForm({ ...formState, [e.target.name]: e.target.value  })
+  }
 
-    // update the form state as the user types
-    function onChange(e) {
-        setForm({ ...formState, [e.target.name]: e.target.value })
-    }
-
-    return (
-        <div style={{ padding: 30 }}>
-            <input
-                onChange={onChange}
-                placeholder="Name"
-                name="name"
-                value={formState.name}
-            />
-            <input
-                onChange={onChange}
-                placeholder="Message"
-                name="message"
-                value={formState.message}
-            />
-            <button onClick={saveMessage}>Send Message</button>
-            {state.messages.map(message => (
-                <div key={message.createdAt}>
-                    <h2>{message.message}</h2>
-                    <h3>From: {message.name}</h3>
-                    <p>Date: {message.createdAt}</p>
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div style={{ padding: 30 }}>
+      <input
+        onChange={onChange}
+        placeholder="Name"
+        name="name"
+        value={formState.name}
+      />
+      <input
+        onChange={onChange}
+        placeholder="Message"
+        name="message"
+        value={formState.message}
+      />
+      <button onClick={saveMessage}>Send Message</button>
+      <button onClick={deleteMessage}>Delete Message</button>
+      {
+        state.messages.map(message => (
+          <div key={message.createdAt}>
+            <h2>{message.message}</h2>
+            <h3>From: {message.name}</h3>
+            <p>Date: {message.createdAt}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
 }
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const Gun = require('gun');
 const app = express()
-const port = 3003
-const hostname = '0.0.0.0'
+const port = 3005
+const hostname = '192.168.1.122'
 app.use(Gun.serve);
 
-const server = app.listen(port, hostname => {
-    console.log(`P2P Messaging app listening on http://${hostname}:${port}. Try ifconfig -a to get ip address`)
-})
+const server = app.listen(port, hostname)
 
 Gun({ web: server })
